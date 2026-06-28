25d6e6a0c3b903942d9e4a40e8e16c0a1baa6a5a
Elvis Gastelum <elvisgastelum@outlook.com>
2026-06-27 18:32:14 -0700

    denvenv: adding typescript-language-server

--- devenv.nix
+++ devenv.nix
@@ -28,6 +28,7 @@
   # Optional: Add utility packages
   packages = [
     pkgs.nodejs_22 # Or latest supported version
+		pkgs.typescript-language-server # TypeScript Language Server
   ];
 
   # See full reference at https://devenv.sh/reference/options/
